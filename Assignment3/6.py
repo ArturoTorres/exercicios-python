1f92a0526126f27c30d34f0a6393517717676714
tom buckley <buckley.tom@gmail.com>
2013-07-27 17:11:30 +0200

    finished problem 6 and added own geojson for washington, dc national mall

--- Assignment3/6.py
+++ Assignment3/6.py
@@ -2,7 +2,7 @@ import math
 import json
 import pprint
 
-json_file = open("polys.json")
+json_file = open("usa_nationalmall.geojson")
 
 data = json.load(json_file)
 
@@ -11,11 +11,13 @@ def area(vertices):
 	y = 1
 	a = 0
 	for i in range(len(vertices)-1):
-		a += vertices[i][x]*vertices[i+1][y]-vertices[i][y]*vertices[i+1][x]
-		print vertices[i][x]*vertices[i+1][y]-vertices[i][y]*vertices[i+1][x]
-
-	print a
+		a += (vertices[i][x]*vertices[i+1][y])-(vertices[i][y]*vertices[i+1][x])
+	a += (vertices[-1][x]*vertices[0][y] - vertices[-1][y]*vertices[0][x])
+	a = a/2
+	return abs(a)
 
 for item in data:
 	for c in item["geometry"]["coordinates"]:
-		area(c)
+		print "polygon area:",area(c)
+
+
