931262143bdc273f62406b0f886643460545c65b
ArturoTorres <arturo.torres@uni-muenster.de>
2013-07-28 15:03:59 +0100

    Update 4.py
    another option

--- Assignment3/4.py
+++ Assignment3/4.py
@@ -16,3 +16,27 @@ for feature in data["features"]:
 		print " "
 
 json_file.close
+
+
+# another option
+import json
+file_name = "feature_collection.geojson"
+f = open(file_name, "r") # r = read, w = write
+document = f.read()
+
+as_dict = json.loads(document)
+
+level = ""
+def print_keys(obj, level):
+		if(type(obj) == type({})):
+			lev = level + "."
+			for k, v in obj.iteritems():
+				print lev, k, v
+				#print k, v
+				print_keys(v, lev)
+		elif (type(obj)== type([])):
+			lev = level + "."
+			for item in obj:
+				print_keys(item, lev)
+
+print_keys(as_dict, level)
